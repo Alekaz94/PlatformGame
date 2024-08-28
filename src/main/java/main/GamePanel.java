efa0013693543ec0f424162107b0c47c349231ec
alexandroskazalis <alexandros.kazalis@hotmail.com>
2024-08-28 16:08:12 +0200

    Created a small rectangle that is movable with WASD and with the mouse.

---
+++ src/main/java/main/GamePanel.java
@@ -0,0 +1,41 @@
+package main;
+
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    private MouseInputs mouseInputs;
+    private int xDelta = 100;
+    private int yDelta = 100;
+
+    public GamePanel() {
+        mouseInputs = new MouseInputs(this);
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+    }
+
+    public void setRectPosition(int x, int y) {
+        this.xDelta = x;
+        this.yDelta = y;
+    }
+
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+    }
+
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.fillRect(xDelta, yDelta, 200, 50);
+        repaint();
+    }
+}
